bc466b5bb9cf9944433372e9413005f8ac6d1f5b
ljy <lijingyue@mzhtechnologies.com>
2019-09-24 18:08:44 +0800

    add components circle && configuration && instance

---
+++ src/components/circleProgress.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="box">
+    <el-container class="detail-wrap">
+      <el-row type="flex"
+              align="bottom">
+        <el-col class="detail-number">{{number}}</el-col>
+        <el-col class="detail-unit">{{unit}}</el-col>
+      </el-row>
+    </el-container>
+    <svg width="130"
+         height="130"
+         viewbox="0 0 130 130">
+      <circle cx="65.5"
+              cy="65.5"
+              r="60.5"
+              stroke-width="5"
+              stroke="#F3F3F3"
+              fill="none">
+      </circle>
+      <circle cx="65.5"
+              cy="65.5"
+              r="60.5"
+              stroke-width="5"
+              :stroke="color"
+              fill="none"
+              transform="matrix(0,-1,1,0,0,130)"
+              :stroke-dasharray="stroke">
+      </circle>
+    </svg>
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: '#54A8F7',
+    },
+    show: {
+      type: Number,
+      default: 500,
+    },
+    total: {
+      type: Number,
+      default: 1000,
+    },
+    number: {
+      type: Number,
+      default: 12,
+    },
+    unit: {
+      type: String,
+      default: 'M',
+    },
+  },
+  data() {
+    return {
+      stroke: '0 1000',
+    };
+  },
+  mounted() {
+    const percent = this.show / this.total;
+    const perimeter = Math.PI * 2 * 60.5;
+    this.stroke = `${perimeter * percent} ${perimeter * (1 - percent)}`;
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.box {
+  background-color: #fff;
+  height: 130px;
+  width: 130px;
+  position: relative;
+}
+
+.detail-wrap {
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 130px;
+  width: 130px;
+}
+
+.detail-number {
+  color: #333;
+  font-size: 36px;
+  font-weight: bold;
+}
+
+.detail-unit {
+  color: #333;
+  font-size: 14px;
+  margin-bottom: 6px;
+  margin-left: 7px;
+}
+
+svg {
+  transform: rotate(180deg);
+  -webkit-transform: rotate(180deg);
+}
+
+circle {
+  transition: stroke-dasharray 1.5s;
+  -webkit-transition: stroke-dasharray 1.5s;
+}
+</style>
---
+++ src/components/instance.vue
@@ -0,0 +1,140 @@
+<template>
+  <el-card shadow="hover">
+    <el-row type="flex"
+            justify="space-between">
+      <el-col class="title left">{{title}}</el-col>
+      <el-col :class="[settled ? 'settled' : 'unsettled', 'status']">{{status}}</el-col>
+    </el-row>
+    <el-row type="flex"
+            class="loss-row">
+      <el-col class="loss">最近实例Loss</el-col>
+      <el-col class="loss">平均实例Loss</el-col>
+    </el-row>
+    <el-row type="flex">
+      <el-col class="lately">{{lately}}</el-col>
+      <el-col class="average">{{average}}</el-col>
+    </el-row>
+    <el-row type="flex"
+            class="bottom"
+            justify="space-between">
+      <el-col class="left">训练时长：{{traning}}</el-col>
+      <el-col class="right">预计部署时长：{{estimate}}</el-col>
+    </el-row>
+  </el-card>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '第一次实例-1908231122',
+    },
+    settled: {
+      type: Boolean,
+      default: true,
+    },
+    lately: {
+      type: Number,
+      default: 0.1,
+    },
+    average: {
+      type: Number,
+      default: 0.3,
+    },
+    traning: {
+      type: String,
+      default: '1小时05分钟',
+    },
+    estimate: {
+      type: String,
+      default: '35分钟',
+    },
+  },
+  computed: {
+    status() {
+      return this.settled ? '已部署' : '待部署';
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.el-card {
+  height: 148px;
+  width: 414px;
+  background-color: #fff;
+  border-radius: 6px;
+  position: relative;
+}
+
+/deep/.el-card__body {
+  padding: 12px;
+}
+
+.loss-row {
+  margin-top: 10px;
+  margin-bottom: 9px;
+}
+
+.left {
+  text-align: left;
+}
+
+.right {
+  text-align: right;
+}
+
+.title {
+  font-size: 14px;
+  font-weight: 400;
+  color: #333;
+}
+
+.status {
+  width: 48px;
+  height: 18px;
+  border-radius: 4px;
+  font-size: 12px;
+  line-height: 17px;
+  color: #fff;
+  text-align: center;
+}
+
+.settled {
+  background: #70b40f;
+}
+
+.unsettled {
+  background: #ffc25d;
+}
+
+.loss {
+  font-size: 14px;
+  color: #333;
+  text-align: left;
+}
+
+.lately {
+  color: #333;
+  font-size: 32px;
+  text-align: left;
+  line-height: 38px;
+}
+
+.average {
+  color: #333;
+  font-size: 32px;
+  font-weight: bold;
+  line-height: 45px;
+  text-align: left;
+}
+
+.bottom {
+  width: 390px;
+  position: absolute;
+  bottom: 12px;
+  font-size: 12px;
+  color: #999;
+}
+</style>
